47f2f8a598e0404c8b6976268ec2164462a5dd92
andres <andres@andres-H61ML>
2022-02-14 12:40:06 -0300

    Actualizar characte_dialogs.tres, lvl_5_player.tscn y 5 más archivos...

--- niveles/nivel_8_transicion_de_escenas/GLOBAL_DATA.gd
+++ niveles/nivel_8_transicion_de_escenas/GLOBAL_DATA.gd
@@ -4,9 +4,9 @@ extends Node
 var next_lvl_door_indx: int = -1			# Indice de puerta donde aparecer
 											# -1 si no usa una puerta. (main_lvl)
 var picked_items: Array = []				# Lista de items conseguidos
-var score: int = 0							# Puntaje
-var total_health: int = 10					# Vida Total
-var health: int = 10						# Vida acutal
+var score: 			int = 0					# Puntaje
+var total_health: 	int = 10				# Salud Total
+var health: 		int = 10				# Salud Actual
 
 func add_to_picked_item_list(_id:String) -> void:
 	picked_items.push_front(_id)
